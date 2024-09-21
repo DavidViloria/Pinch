ec8b1c64cc1c3041c15dba5b18ab34fba74e7ce7
David Viloria Ortega <david.viloria0424@gmail.com>
2024-09-21 16:04:17 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by David Viloria Ortega on 21/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
