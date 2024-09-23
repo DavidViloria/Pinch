10efbe7283d901e289ff8b0a19ed452042c01e00
David Viloria Ortega <david.viloria0424@gmail.com>
2024-09-22 19:26:34 -0500

    Improved drawer behavior: added horizontal ScrollView and adjusted offset for visibility when closed

---
+++ Pinch/Data/PageDate.swift
@@ -0,0 +1,8 @@
+//
+//  PageDate.swift
+//  Pinch
+//
+//  Created by David Viloria Ortega on 22/09/24.
+//
+
+import Foundation
---
+++ Pinch/Model/PageModel.swift
@@ -0,0 +1,8 @@
+//
+//  PageModel.swift
+//  Pinch
+//
+//  Created by David Viloria Ortega on 22/09/24.
+//
+
+import Foundation
